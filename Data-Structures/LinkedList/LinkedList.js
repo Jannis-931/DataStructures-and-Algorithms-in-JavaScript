22e1d6decf4ee7e2be6fb3e7e98b335bcb7d2a14
sajib-mandal <sajibmandalcse@gmail.com>
2023-05-01 14:50:32 +0530

    linkedlist file added

---
+++ Data-Structures/LinkedList/LinkedList.js
@@ -0,0 +1,16 @@
+import LinkedListNode from "./LinkedListNode";
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    // adds an element at the end of the list
+    add(data) {
+        const node = LinkedListNode(data);
+
+        if (this.next)
+    }
+}
