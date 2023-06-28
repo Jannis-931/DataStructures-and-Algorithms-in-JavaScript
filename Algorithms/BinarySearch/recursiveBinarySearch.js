492454e9351b5ef53fba23df6682ee7086038805
sajib-mandal <sajibmandalcse@gmail.com>
2023-06-28 10:17:21 +0530

    readme.md file updated

--- Algorithms/BinarySearch/recursiveBinarySearch.js
+++ Algorithms/BinarySearch/recursiveBinarySearch.js
@@ -1,24 +1,21 @@
-const recursiveBS = (arr, target, start, end) => {
-  if (start > end) {
-    return -1; // Target element not found
+const recursiveBS = function(arr, target, s, e) {
+  if (s > e) {
+    return -1;
   }
 
-  const mid = Math.floor((start + end) / 2);
+  const m = Math.floor((s + e) / 2);
 
-  if (arr[mid] === target) {
-    return mid; // Target element found
-  } else if (arr[mid] < target) {
-    return recursiveBS(arr, target, mid + 1, end); // Search in the right half
-  } else {
-    return recursiveBS(arr, target, start, mid - 1); // Search in the left half
+  if (arr[m] === target) {
+    return m;
+  } 
+
+  if (target < arr[m]) {
+    return recursiveBS(arr, target, s, m - 1);
   }
-};
-
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const target = 9;
-const result = recursiveBS(arr, target, 0, arr.length - 1);
-if (result !== -1) {
-  console.log('Element found at index:', result);
-} else {
-  console.log('Element not found.');
+  return recursiveBS(arr, target, m + 1, e);
 }
+
+const arr = [1, 2, 3, 4, 55, 66, 78];
+const target = 69;
+const result = recursiveBS(arr, target, 0, arr.length - 1);
+console.log(result);
--- Recursion/RECURSION.md
+++ Recursion/RECURSION.md
@@ -2,3 +2,26 @@
 
 <img src="https://github.com/sajib-mandal/DataStructures-and-Algorithms-in-JavaScript/blob/main/images/recursion.png" width="400" height="420">
 
+
+**`Note`**:
+How to understand and approach a problem:
+- Identify if you can break down problem into smaller problems.
+- Write the recurence relation if needed. ex: `fibo(n) = fibo(n - 1) + fibo(n - 2)`.
+- Draw the recursive tree.
+- About the tree:
+    - See the flow of functions, how they are getting in stack.
+    - Identify and focus on left tree calls and right tree calls.
+    - Draw the tree and pointer again and again using pen and paper.
+    - Use a dibugger to see the flow.
+- See how the values and what types of values (int, string, etc.) are returned at each step. See where the function call will come out. In the end will come out of the main function.
+
+**`Variables`**: vvvvi
+Variables:
+1. Arguments
+2. Return type
+3. Body of function
+
+Types of Recurence Relation:
+1. Linear recurence relation -> fibo
+2. Divide & conquere recurence relation -> binary search because divide the problem two half.
+ 
